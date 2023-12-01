ffcec34b95a86b1482a1449758d9737bd0766cae
Alain Perez <aperez@mondragon.edu>
2023-12-01 14:22:06 +0100

    WEB INTERFACE

--- main.py
+++ main.py
@@ -1,12 +1,98 @@
 import json
 import os
 from functools import reduce
-import pandas as pd
+# import pandas as pd
 
 import pytesseract
 from PIL import Image
 from fuzzywuzzy import fuzz
 import re
+from fastapi import FastAPI, WebSocket
+from fastapi.responses import FileResponse, HTMLResponse
+
+with open('jsons/cards_all_2023-11-28.json') as f:
+    data = json.load(f)
+
+app = FastAPI()
+
+
+@app.get("/")
+async def index():
+    """return HTMLResponse with index.html"""
+    return FileResponse("index.html")
+
+
+@app.get("/images/not_processed/{image_name}")
+async def get_image(image_name):
+    """return image"""
+    return FileResponse(f"./images/not_processed/{image_name}")
+
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    await process_images(websocket)
+    await websocket.send_text(json_message("WS finished"))
+    # while True:
+    #     data = await websocket.receive_text()
+    #     print(data)
+    #     await websocket.send_text(f"Message text was: {data}")
+
+
+async def process_images(websocket: WebSocket):
+    await websocket.send_text(json_message("Processing images..."))
+    directory = "./images/not_processed"
+    image_files = [f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))]
+    num_images = len(image_files)
+    for image_file in image_files:
+        file_name = os.path.basename(image_file)
+        image = Image.open(os.path.join(directory, image_file))
+        processed_text, similarity_scores, text_to_compare_list = process_image_file(image)
+
+        sorted_similarity_scores = sorted(similarity_scores.items(), key=lambda x: x[1], reverse=True)
+
+        best_match_code = sorted_similarity_scores[0][0]
+        #best_match_card = next(card for card in data if card['code'] == best_match_code)
+        text = "starting"
+        while text != 'yes' and text != 'skip' and len(sorted_similarity_scores) > 0:
+            best_matching = sorted_similarity_scores.pop(0)
+            best_match_code = best_matching[0]
+            result_json = json_result(
+                file_name,
+                best_match_code,
+                similarity_scores[best_match_code],
+                processed_text
+            )
+            await websocket.send_text(result_json)
+
+            text = await websocket.receive_text()
+
+        if text == 'yes':
+            image_is_card(image, best_match_code)
+
+        await websocket.send_text(json_message(f"Image processed {text}. {num_images} images left."))
+        num_images -= 1
+    await websocket.send_text(json_message("Processing finished..."))
+
+
+def json_message(message):
+    return json.dumps({"type": "message", "message": message})
+
+
+def image_is_card(image, card_code):
+    '''Copy image to processed folder and name it as card code'''
+    extension = image.format.lower()
+    image.save(f"./images/processed/{card_code}.{extension}")
+
+
+def json_result(input_image, result_code, result_score, text):
+    return json.dumps({
+        "type": "result",
+        "input_image": input_image,
+        "result_code": result_code,
+        "result_score": result_score,
+        "text": text}
+    )
 
 
 def clean_text(text):
@@ -45,84 +131,10 @@ def process_image_file(image):
     return processed_text, similarity_scores, text_to_compare_list
 
 
-def process_images_and_save_to_excel(image_files, data):
-    similarity_scores = {}
-    text_to_compare_list = {}
-    # Create an empty list to store the data for each image
-    data_list = []
+def add_card_data_to_compare(input_card, input_text_to_compare=""):
+    keys_to_check = ["name", "flavor", "traits", "text", "scheme_text", "attack_text", "card_set_name", "boost_text"]
 
-    for file_path in image_files:
-        file_name = os.path.basename(file_path)
-        image = Image.open(os.path.join(directory, file_name))
-
-        # Get the top 10 matches for the image
-        top_match_codes, top_match_cards, similarity_scores, text_to_compare_list = get_top_matches(image, data, num_matches=10)
-
-        # For each match, create a dictionary and append it to the list
-        for match_code, match_card in zip(top_match_codes, top_match_cards):
-            data_dict = {
-                'Image Name': file_name,
-                'Card Code': match_code,
-                'Similarity Score': similarity_scores[match_code],
-                'Text to Compare': text_to_compare_list[match_code]
-            }
-            data_list.append(data_dict)
-
-    # Convert the list of dictionaries to a DataFrame
-    df = pd.DataFrame(data_list)
-
-    # Write the DataFrame to an Excel file
-    df.to_excel('image_matches.xlsx', index=False)
-
-
-def get_top_matches(image, data, num_matches=10):
-    similarity_scores, text_to_compare_list = process_image_file(image)
-
-    # Sort the similarity scores in descending order and get the top num_matches
-    top_match_codes = sorted(similarity_scores, key=similarity_scores.get, reverse=True)[:num_matches]
-
-    # Find the cards corresponding to the top num_matches match codes
-    top_match_cards = [next(card for card in data if card['code'] == match_code) for match_code in top_match_codes]
-
-    return top_match_codes, top_match_cards, similarity_scores, text_to_compare_list
-
-
-if __name__ == "__main__":
-    # Load JSON data
-    with open('jsons/cards_all_2023-11-28.json') as f:
-        data = json.load(f)
-
-
-    def add_card_data_to_compare(input_card, input_text_to_compare=""):
-        keys_to_check = ["name", "flavor", "traits", "text", "scheme_text", "attack_text", "card_set_name", "boost_text"]
-
-        for key in keys_to_check:
-            if key in input_card.keys() and input_card[key] is not None:
-                input_text_to_compare += " " + clean_text(input_card[key].lower())
-        return input_text_to_compare
-
-
-    directory = "./images"
-    image_files = [f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))]
-
-    #process_images_and_save_to_excel(image_files, data)
-    for file_path in image_files:
-        file_name = os.path.basename(file_path)
-        image = Image.open(os.path.join(directory, file_name))
-
-        processed_text, similarity_scores, text_to_compare_list = process_image_file(image)
-
-        # top_match_codes, top_match_cards = get_top_matches(image, data, num_matches=3)
-        best_match_code = max(similarity_scores, key=similarity_scores.get)
-        best_match_card = next(card for card in data if card['code'] == best_match_code)
-
-        print(f"\n---------------------------------------------\n{file_name}\n")
-        print(f"Best match: {best_match_code}\nScore: {similarity_scores[best_match_code]}\n")
-        print(f"Text from image:\n{processed_text}\n")
-        print(f"Compared with:\n{text_to_compare_list[best_match_card['code']]}")
-        # if "name" in best_match_card.keys():
-        #     print(f"Name: {best_match_card['name']}")
-        # if "text" in best_match_card.keys():
-        #     print(f"Text: {best_match_card['text']}")
-        # if "flavor" in best_match_card.keys():
-        #     print(f"Flavor: {best_match_card['flavor']}")
+    for key in keys_to_check:
+        if key in input_card.keys() and input_card[key] is not None:
+            input_text_to_compare += " " + clean_text(input_card[key].lower())
+    return input_text_to_compare
